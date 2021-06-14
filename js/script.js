16ef3ccf70faa72027cf0dc9870f39df93c89e49
Springbank9 <isabellahay7@gmail.com>
2021-06-14 17:00:26 +0000

    this is another go at a good template

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+(function() {
+  //Image Credit - https://medium.com/
+
+  //https://github.com/andreasstorm
+  $(window).scroll(function() {
+    var oVal;
+    oVal = $(window).scrollTop() / 240;
+    return $(".blur").css("opacity", oVal);
+  });
+
+}).call(this);
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiPGFub255bW91cz4iXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBRWdDO0VBQUE7OztFQUVoQyxDQUFBLENBQUUsTUFBRixDQUFTLENBQUMsTUFBVixDQUFpQixRQUFBLENBQUEsQ0FBQTtBQUNqQixRQUFBO0lBQUUsSUFBQSxHQUFRLENBQUEsQ0FBRSxNQUFGLENBQVMsQ0FBQyxTQUFWLENBQUEsQ0FBQSxHQUF3QjtXQUNoQyxDQUFBLENBQUUsT0FBRixDQUFVLENBQUMsR0FBWCxDQUFlLFNBQWYsRUFBMEIsSUFBMUI7RUFGZSxDQUFqQjtBQUZnQyIsInNvdXJjZXNDb250ZW50IjpbIiNJbWFnZSBDcmVkaXQgLSBodHRwczovL21lZGl1bS5jb20vXG5cbiNodHRwczovL2dpdGh1Yi5jb20vYW5kcmVhc3N0b3JtXG5cbiQod2luZG93KS5zY3JvbGwgLT5cbiAgb1ZhbCA9ICgkKHdpbmRvdykuc2Nyb2xsVG9wKCkgLyAyNDApXG4gICQoXCIuYmx1clwiKS5jc3MgXCJvcGFjaXR5XCIsIG9WYWwiXX0=
+//# sourceURL=coffeescript
+
 jQuery(document).ready(function($){
 	var timelines = $('.cd-horizontal-timeline'),
 		eventsMinDistance = 60;
