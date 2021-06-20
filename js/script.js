573546d5dbfee14d358d6fd12bc2f871b03b7365
Springbank9 <isabellahay7@gmail.com>
2021-06-20 16:22:56 +0000

    my arms are getting pins and needles

--- js/script.js
+++ js/script.js
@@ -24,67 +24,78 @@ $(document).ready(function() {
 		  $(".menu > ul").removeClass('show-on-mobile');
 		});
 
-
-        {
-	class SliderClip {
-		constructor(el) {
-			this.el = el;
-			this.Slides = Array.from(this.el.querySelectorAll('li'));
-			this.Nav = Array.from(this.el.querySelectorAll('nav a'));
-			this.totalSlides = this.Slides.length;
-			this.current = 0;
-			this.autoPlay = true; //true or false
-			this.timeTrans = 4000; //transition time in milliseconds
-			this.IndexElements = [];
-
-			for(let i=0;i<this.totalSlides;i++) {
-				this.IndexElements.push(i);
-			}
-
-			this.setCurret();
-			this.initEvents();
-		}
-		setCurret() {
-			this.Slides[this.current].classList.add('current');
-			this.Nav[this.current].classList.add('current_dot');
-		}
-		initEvents() {
-			const self = this;
-
-			this.Nav.forEach((dot) => {
-				dot.addEventListener('click', (ele) => {
-					ele.preventDefault();
-					this.changeSlide(this.Nav.indexOf(dot));
-				})
-			})
-
-			this.el.addEventListener('mouseenter', () => self.autoPlay = false);
-			this.el.addEventListener('mouseleave', () => self.autoPlay = true);
-
-			setInterval(function() {
-				if (self.autoPlay) {
-					self.current = self.current < self.Slides.length-1 ? self.current + 1 : 0;
-					self.changeSlide(self.current);
-				}
-			}, this.timeTrans);
-
-		}
-		changeSlide(index) {
-
-			this.Nav.forEach((allDot) => allDot.classList.remove('current_dot'));
-
-			this.Slides.forEach((allSlides) => allSlides.classList.remove('prev', 'current'));
-
-			const getAllPrev = value => value < index;
-
-			const prevElements = this.IndexElements.filter(getAllPrev);
-
-			prevElements.forEach((indexPrevEle) => this.Slides[indexPrevEle].classList.add('prev'));
-
-			this.Slides[index].classList.add('current');
-			this.Nav[index].classList.add('current_dot');
-		}
-	}
-
-	const slider = new SliderClip(document.querySelector('.slider'));
+var video;
+function playVideo() {
+  console.log("playVideo called");
+  video.play();
 }
+
+(function($){
+
+  
+  Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+      get: function(){
+          console.log(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+          return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+      }
+  })
+
+  // Once DOM is ready.
+  $(document).ready(function(){
+    video = document.getElementById("bg-video");
+    $('#bg-video').on('loadstart', function (event) {
+      $('#loader').fadeIn();
+    });
+
+    $('#bg-video').on('canplay', function(event) {
+      setTimeout(function() {
+        if (video.playing) {
+            $('#loader').fadeOut();
+        }
+
+        if (!video.playing) {
+            $('#loader').fadeOut();
+            if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) {
+              $('#btn-play-bg').show();
+            }
+        }
+      }, 1000);
+    });
+
+    $('#btn-play-bg').on('click', function() {
+      $(this).hide();
+      playVideo();
+    });
+
+    if(!$('#container').hasClass('is-project')) {
+      return;
+    }
+  	var $quicklinks = $('.projects-quicklinks');
+  	var $quicklinksOffset = $quicklinks.offset().top;
+  	var $quicklinksHeight = $quicklinks.height();
+
+  	if (($quicklinksOffset + $quicklinksHeight) < $(window).height()) {
+  		$quicklinks.addClass('sticky');
+  	}
+
+  	$(document).on('scroll', function(e) {
+
+  		if ($(window).width() >= 768) {
+  			
+	  		var $scrollDistance = $(document).scrollTop();
+	  		var $windowHeight = $(window).height();
+	  		var visibleDistance = $windowHeight + $scrollDistance;
+	  		var quicklinksBottom = $quicklinksOffset + $quicklinksHeight;
+
+	  		if (visibleDistance >= quicklinksBottom  && !$quicklinks.hasClass('sticky')) {
+	  			$quicklinks.addClass('sticky');
+	  		} else if (visibleDistance < quicklinksBottom && $quicklinks.hasClass('sticky')) {
+	  			$quicklinks.removeClass('sticky');
+	  		}
+  		}
+  	});
+
+  });
+  
+
+}(jQuery));
